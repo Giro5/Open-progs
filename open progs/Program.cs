e7fd89829032103ef157dd5832c7ba631519b30d
Nikita <39918253+Giro5@users.noreply.github.com>
2018-09-16 15:23:44 +0500

    Add files via upload

---
+++ open progs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace open_progs
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Process.Start($@"C:\Users\{Environment.UserName}\source\repos");
+            Application.Exit();
+        }
+    }
+}
